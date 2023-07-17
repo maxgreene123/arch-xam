11474d0a1a2f669789b3c17d253bb44d08101987
Max Greene <101725496+maxgreene123@users.noreply.github.com>
2023-07-17 12:47:27 -0400

    Update install-yay.sh

--- install-yay.sh
+++ install-yay.sh
@@ -1,2 +1,5 @@
-pacman -S --needed git base-devel && mkdir /home/$username/github-projects && cd /home/$username/github-projects && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+#update system and install yay
+sudo pacman -S --needed git base-devel && mkdir ~/github-projects && cd ~/github-projects && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# install essential programs
+yay -S feh picom thunar thunar-archive-plugin firefox neofetch ly gpg spotify discord-screenaudio sublime-text-4 lxappearance alsa-tools unzip wget pulseaudio xorg pavucontrol cmake webkit2gtk xorg-xinit libx11 libxinerama libxft -y
 cd ~
