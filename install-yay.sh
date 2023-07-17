5080d44421df0de71f2ba89a5cade6294dc6ef2a
Max Greene <101725496+maxgreene123@users.noreply.github.com>
2023-07-17 12:35:17 -0400

    Update install-yay.sh

--- install-yay.sh
+++ install-yay.sh
@@ -1,1 +1,2 @@
-
+pacman -S --needed git base-devel && mkdir /home/$username/github-projects && cd /home/$username/github-projects && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd ~
